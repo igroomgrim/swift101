bee8f81b57e7a3a2940ebdc7d6140dc3643d7e18
Anak Mirasing <thaihooligan@hotmail.com>
2015-10-13 23:12:44 +0700

    Day 91 - Strings in Swift 2 (Collection of Character Values)

--- Swift101_Playground.playground/Contents.swift
+++ Swift101_Playground.playground/Contents.swift
@@ -8,3 +8,19 @@ userAddress.buildingName
 userAddress.buildingNumber
 userAddress.street
 userAddress.displayDetail()
+
+// Day 91 - Strings in Swift 2
+var letters: [Character] = ["A", "R", "S", "E", "N", "A", "L"]
+var string: String = String(letters)
+
+print(letters.count)
+print(string)
+print(string.characters.count)
+
+let registeredTrademark: Character = "\u{00AE}"
+string.append(registeredTrademark)
+print(string.characters.count)
+print(string.characters.last!)
+print(string.characters.first!)
+
+string.characters.contains("S")
