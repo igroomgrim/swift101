d0241be55d3d8ea6136012cf2d27986675162fc1
Anak Mirasing <thaihooligan@hotmail.com>
2015-10-15 23:39:54 +0700

    Day 89 - Sets in Swift

--- Swift101_Playground.playground/Contents.swift
+++ Swift101_Playground.playground/Contents.swift
@@ -3,13 +3,16 @@
 import UIKit
 
 // Day 92 - Import your class into XCODE 7 Playground
+/*
 let userAddress = Address(buildingName: "Shanghai Tower", buildingNumber: "BD01", street: "Shanghai Road")
 userAddress.buildingName
 userAddress.buildingNumber
 userAddress.street
 userAddress.displayDetail()
+*/
 
 // Day 91 - Strings in Swift 2
+/*
 var letters: [Character] = ["A", "R", "S", "E", "N", "A", "L"]
 var string: String = String(letters)
 
@@ -24,8 +27,10 @@ print(string.characters.last!)
 print(string.characters.first!)
 
 string.characters.contains("S")
+*/
 
 // Day 90 - UIKit on playground
+/*
 let maView = MaView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
 maView.backgroundColor = UIColor.blueColor()
 maView
@@ -38,3 +43,27 @@ let maButton = MaButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
 maButton.backgroundColor = UIColor.greenColor()
 maButton.layer.cornerRadius = 4
 maButton
+*/
+
+// Day 89 - Sets
+var characters = Set<Character>()
+characters.insert("R")
+characters.insert("O")
+characters.insert("C")
+characters.insert("K")
+characters
+
+var pointSet: Set<Int> = [1,2,3,4,5,6,7,8,9]
+for point in pointSet {
+    print("\(point)")
+}
+
+for point in pointSet.sort() {
+    print("\(point)")
+}
+
+var setOne: Set<Int> = [4,5,6,7,8,8]
+var setTwo: Set<Int> = [1,2,3,4,5]
+
+setOne.intersect(setTwo)
+setOne.subtract(setTwo)
