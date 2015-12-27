65dfb291b3be52e7d47a64c9e483d0fbed8104c1
Anak Mirasing <thaihooligan@hotmail.com>
2015-12-27 20:00:03 +0700

    Day 23 - Three computed properties case of the string

---
+++ swift101/Day23_CaseOfTheString.swift
@@ -0,0 +1,21 @@
+//
+//  Day23_CaseOfTheString.swift
+//  swift101
+//
+//  Created by Anak Mirasing on 12/27/2558 BE.
+//  Copyright © 2558 iGROOMGRiM. All rights reserved.
+//
+
+import UIKit
+
+class Day23_CaseOfTheString: NSObject {
+    
+    let myString = "case of the STRING"
+    
+    func printString() {
+        print("Capitalized String : " + myString.capitalizedString)
+        print("Lowercase String : " + myString.lowercaseString)
+        print("Uppercase String : " + myString.uppercaseString)
+    }
+    
+}
