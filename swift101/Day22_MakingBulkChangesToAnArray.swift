e3400ac8b7dbb9c7db64dd7ae7d60a6742524a08
Anak Mirasing <thaihooligan@hotmail.com>
2015-12-29 23:37:03 +0700

    Day 21 - Making bulk changes to an array in Swift (Fixed)

--- swift101/Day22_MakingBulkChangesToAnArray.swift
+++ swift101/Day22_MakingBulkChangesToAnArray.swift
@@ -8,12 +8,15 @@
 
 import UIKit
 
-class Day22_MakingBulkChangesToAnArray: NSObject {
+class Day21_MakingBulkChangesToAnArray: NSObject {
     
     var myArray = [1,2,3,4,5,6,7,8,9,10]
     
     func changeToArray(a: Int, b: Int, c: Int) {
         myArray[3...5] = [44,55,66]
         // myArray contains [1,2,3,44,55,66,7,8,9,10]
+        
+        myArray[0...2] = [a,b,c]
+        // myArray contains [a,b,c,44,55,66,7,8,9,10]
     }
 }
