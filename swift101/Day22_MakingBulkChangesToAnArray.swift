089a0860d5d11744206cab852d839301a55f6d3f
Anak Mirasing <thaihooligan@hotmail.com>
2015-12-31 14:43:45 +0700

    Day 19 - The sort algorithm sorts the array in place.

--- swift101/Day22_MakingBulkChangesToAnArray.swift
+++ swift101/Day22_MakingBulkChangesToAnArray.swift
@@ -29,4 +29,17 @@ class Day21_MakingBulkChangesToAnArray: NSObject {
         myArray.sortInPlace(){ $1 < $0 }
         // myArray contains [10,9,8,7,6,5,4,3,2,1]
     }
+    
+    // Day 19 - The sort algorithm sorts the array in place.
+    func sortArray() {
+        let arraySorted = myArray.sort(){ $0 < $1 }
+        print(arraySorted)
+        // arraySorted contains [1,2,3,4,5,6,7,8,9,10]
+        
+        let arrayReSorted = myArray.sort(){ $1 < $0 }
+        print(arrayReSorted)
+        // myArray contains [10,9,8,7,6,5,4,3,2,1]
+        
+        
+    }
 }
