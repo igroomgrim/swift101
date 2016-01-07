aac7e99e5cd36ace85b0a50e93c62b4fafd416b3
Anak Mirasing <thaihooligan@hotmail.com>
2016-01-07 22:11:02 +0700

    Day 12 - Check string contains another string

---
+++ swift101/Day12_CheckContainString.swift
@@ -0,0 +1,16 @@
+//
+//  Day12_CheckContainString.swift
+//  swift101
+//
+//  Created by Anak Mirasing on 1/7/2559 BE.
+//  Copyright © 2559 iGROOMGRiM. All rights reserved.
+//
+
+import UIKit
+
+class Day12_CheckContainString: NSObject {
+    
+    class func containsString(mainString: String, checkString: String) -> Bool {
+        return mainString.containsString(checkString)
+    }
+}
