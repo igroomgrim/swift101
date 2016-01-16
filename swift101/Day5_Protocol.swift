691c4966497a9b6f6601e26c20ce705af57b425e
Anak Mirasing <thaihooligan@hotmail.com>
2016-01-16 23:14:41 +0700

    Day 3 - Delegate in Swift

--- swift101/Day5_Protocol.swift
+++ swift101/Day5_Protocol.swift
@@ -57,3 +57,48 @@ class Yamaha: Motorcycle {
     }
 }
 
+// Day 3 - Delegate
+protocol HackingJob {
+    func printScreen() -> String
+}
+
+class Hunted: HackingJob {
+    var name: String
+    
+    func printScreen() -> String {
+        return "Hack your screen"
+    }
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Hunter {
+    
+    var name: String
+    var thisIsHunted: HackingJob?
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func hacking() -> String {
+        if let hunted = thisIsHunted {
+            return hunted.printScreen()
+        } else {
+            return "NO ONE IS HUNTED"
+        }
+    }
+}
+
+// How to use 
+/*
+let hacker = Hunter(name: "XXX")
+let sarah = Hunted(name: "sarah")
+hacker.thisIsHunted = sarah
+let result = hacker.hacking()
+*/
+
+
+
