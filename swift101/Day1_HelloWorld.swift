66823a9270982ac066881aa635af2bc3d1da1c10
Anak Mirasing <thaihooligan@hotmail.com>
2016-01-18 22:27:33 +0700

    Day 1 - Hello World

---
+++ swift101/Day1_HelloWorld.swift
@@ -0,0 +1,13 @@
+//
+//  Day1_HelloWorld.swift
+//  swift101
+//
+//  Created by Anak Mirasing on 1/18/2559 BE.
+//  Copyright © 2559 iGROOMGRiM. All rights reserved.
+//
+
+import Foundation
+
+func HELLOWORLD() {
+    print("HELLO WORLD")
+}
